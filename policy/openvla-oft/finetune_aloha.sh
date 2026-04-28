2d169d325fc7e66f84cc7820385102fdb65d0bfe
CarlDegio <2536890272@qq.com>
2026-04-28 13:22:19 +0800

    fix dataset name

--- policy/openvla-oft/finetune_aloha.sh
+++ policy/openvla-oft/finetune_aloha.sh
@@ -1,15 +1,15 @@
 # In experiments, global batch size of less than 16 will easily lead to unsuccessful training, where the training and validation 
 # loss would not converge low enough, and the final policy would repeat one trajectory regardless of the visual and language inputs.
-export TORCH_NCCL_HEARTBEAT_TIMEOUT_SEC=7200
-export NCCL_TIMEOUT=7200 
-export NCCL_SOCKET_IFNAME=lo
-export GLOO_SOCKET_IFNAME=lo
-export MASTER_ADDR=127.0.0.1
+# export TORCH_NCCL_HEARTBEAT_TIMEOUT_SEC=7200
+# export NCCL_TIMEOUT=7200 
+# export NCCL_SOCKET_IFNAME=lo
+# export GLOO_SOCKET_IFNAME=lo
+# export MASTER_ADDR=127.0.0.1
 # export NCCL_P2P_LEVEL=0
-torchrun --standalone --nnodes 1 --nproc-per-node 3 \
+torchrun --standalone --nnodes 1 --nproc-per-node 8 \
   vla-scripts/finetune.py \
   --vla_path openvla/openvla-7b \
-  --data_root_dir /home/ubuntu/tensorflow_datasets/ \
+  --data_root_dir /home/lzh/tensorflow_datasets/ \
   --dataset_name robotwin4stack_aloha \
   --run_root_dir ckpts \
   --use_l1_regression False \
@@ -18,7 +18,7 @@ torchrun --standalone --nnodes 1 --nproc-per-node 3 \
   --num_images_in_input 1 \
   --grad_accumulation_steps 1 \
   --use_proprio True \
-  --batch_size 4 \
+  --batch_size 1 \
   --learning_rate 5e-4 \
   --num_steps_before_decay 50000 \
   --max_steps 100005 \
@@ -30,7 +30,7 @@ torchrun --standalone --nnodes 1 --nproc-per-node 3 \
   --lora_rank 32 \
   --wandb_entity "carldegio" \
   --wandb_project "openvla-oft" \
-  --run_id_override "Optional" \
+  --run_id_override "discrete_1frame_prop" \
   --run_id_note some_run_id_note \
   # --resume True \
   # --resume_step 60000 \
